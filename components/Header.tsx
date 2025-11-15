7827e4fcde9e3e6108bd7f609db65b47526bfea8
M. G. <mingjia.guan@gmail.com>
2025-11-14 21:39:08 -0500

    dropdown wip up to navitems

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import NavItems from './NavItems'
+import NavItems from '@/components/NavItems'
+import UserDropdown from '@/components/UserDropdown'
 
 
 const Header = () => {
@@ -13,7 +14,7 @@ const Header = () => {
         <nav className='hidden sm:block'>
           <NavItems />
         </nav>
-        {/* UserDropdown */}
+        <UserDropdown />
       </div>
     </header>
   )
---
+++ components/UserDropdown.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation"
+import { LogOut } from "lucide-react"
+
+import NavItems from '@/components/NavItems'
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuPortal,
+    DropdownMenuSeparator,
+    DropdownMenuShortcut,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function UserDropdown() {
+    const router = useRouter();
+
+    const handleSignOut = async () => {
+        router.push('sign-in');
+    }
+
+    const user = { firstName: 'John', lastName: 'Doe', email: 'johndoe@example.com' };
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+                    <Avatar className="h-8 w-8">
+                        <AvatarImage src='https://avatars.githubusercontent.com/u/244079107?s=400&u=98a0f3431bdef4b8cca75f4158ac335d28729a03&v=4' />
+                        <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                            {user.firstName[0] + user.lastName[0]}
+                        </AvatarFallback>
+                    </Avatar>
+                    <div className="hidden md:flex items-start">
+                        <span className="text-base font-medium text-gray-400">
+                            {user.firstName + " " + user.lastName}
+                        </span>
+                    </div>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56 text-gray-400" align="start">
+                <DropdownMenuLabel>
+                    <div className="flex relative items-center gap-3 py-2">
+                        <Avatar className="h-8 w-8">
+                            <AvatarImage src='https://avatars.githubusercontent.com/u/244079107?s=400&u=98a0f3431bdef4b8cca75f4158ac335d28729a03&v=4' />
+                            <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                                {user.firstName[0] + user.lastName[0]}
+                            </AvatarFallback>
+                        </Avatar>
+                        <div className="flex flex-col">
+                            <span className="text-base font-medium text-gray-500">
+                                {user.firstName + " " + user.lastName}
+                            </span>
+                            <span className="text-sm text-gray-500">
+                                {user.email}
+                            </span>
+                        </div>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator className="bg-gray-600" />
+                <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 class-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+                    <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
+                    Logout
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="bg-gray-600 hidden sm:block" />
+                <nav className="sm:hidden">
+                    <NavItems />
+                </nav>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default UserDropdown
