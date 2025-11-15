31cf2bb171b9fba6c197d88140b68f4630559cc4
M. G. <mingjia.guan@gmail.com>
2025-11-14 20:42:52 -0500

    header wip

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+// import { Button } from "@/components/ui/button"
+import Header from "@/components/Header"
+
+const Home = () => {
+  return (
+    <div className="flex home-wrapper min-h-screen">
+      <Header />
+    </div>
+  )
+}
+
+export default Home
