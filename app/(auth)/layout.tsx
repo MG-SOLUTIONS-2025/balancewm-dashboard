1b312e955c6429a06d6123e30f38e4f0f0b0ee8d
M. G. <mingjia.guan@gmail.com>
2025-11-18 23:46:47 -0500

    auth page wip

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '@/components/Header'
+
+const Layout = ({ children }: { children : React.ReactNode}) => {
+  return (
+    <main className='auth-layout'>
+
+    </main>
+  )
+}
+
+export default Layout
