fd44c5fa032bca5d48763b1e30b3d7f47a84bf57
M. G. <mingjia.guan@gmail.com>
2025-11-20 16:00:47 -0500

    forms wip, country next

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,6 +2,10 @@
 
 import React from 'react'
 import { useForm } from "react-hook-form";
+import InputField from '@/components/forms/InputField';
+import { Button } from '@/components/ui/button';
+import SelectField from '@/components/forms/SelectField';
+import { INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS } from '@/lib/constants';
 
 
 const SignUp = () => {
@@ -35,11 +39,77 @@ const SignUp = () => {
       <h1 className='form-title'>Sign Up & Personalize</h1>
 
       <form onSubmit={handleSubmit(onSubmit)} className='space-y-5' >
-        {/* INPUTS */}
+        <InputField 
+          name='fullname'
+          label='Full Name'
+          placeholder='John Doe'
+          register={register}
+          error={errors.fullName}
+          validation={{ required: 'Full name is required', minLength: 2 }}
+        >
+        </InputField>
 
-        <button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5' >
+        <InputField 
+          name='email'
+          label='Email'
+          placeholder='john.doe@example.com'
+          register={register}
+          error={errors.email}
+          validation={{ required: 'Email is required', pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: 'Email address is required'}}
+        >
+        </InputField>
+
+
+        <InputField 
+          name='password'
+          label='Password'
+          placeholder='Enter a strong password'
+          type='password'
+          register={register}
+          error={errors.password}
+          validation={{ required: 'Password is required', minLength: 8 }}
+        >
+        </InputField>
+
+        {/* Country */}
+
+        <SelectField 
+          name='investmentGoals'
+          label='Investment Goals'
+          placeholder='Select your investment goal'
+          options={INVESTMENT_GOALS}
+          control={control}
+          error={errors.investmentGoals}
+          required
+        />
+
+
+
+        <SelectField 
+          name='riskTolerance'
+          label='Risk Tolerance'
+          placeholder='Select your risk level'
+          options={RISK_TOLERANCE_OPTIONS}
+          control={control}
+          error={errors.riskTolerance}
+          required
+        />
+
+
+
+        <SelectField 
+          name='preferredIndustry'
+          label='Preferred Industry'
+          placeholder='Select your preferred industry'
+          options={PREFERRED_INDUSTRIES}
+          control={control}
+          error={errors.preferredIndustry}
+          required
+        />
+
+        <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5' >
           {isSubmitting ? 'Creating Account' : 'Start Your Investing Journey'}
-        </button>
+        </Button>
       </form>
     </>
   )
