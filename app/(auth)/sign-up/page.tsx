34424a81a392b762a407ffb5883fda067d7fa84e
M. G. <mingjia.guan@gmail.com>
2025-11-20 15:25:51 -0500

    signup wip interface global added

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,39 @@
+'use client';
+
 import React from 'react'
+import { useForm } from "react-hook-form";
+
 
 const SignUp = () => {
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isSubmitting }
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      country: 'US',
+      investmentGoals: 'Growth',
+      riskTolerance: 'medium',
+      preferredIndustry: 'Technology',
+    }, mode: "onBlur"
+  },  );
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
-    <div>SignUpPage</div>
+    <>
+      <h1 className='form-title'>Sign Up & Personalize</h1>
+    </>
   )
 }
 
