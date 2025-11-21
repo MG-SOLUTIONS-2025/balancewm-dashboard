12e0c51f41b327f7728129f3cf592c5c71f2a7ae
M. G. <mingjia.guan@gmail.com>
2025-11-21 11:48:22 -0500

    inngest wip

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,7 +1,8 @@
 import { inngest } from "@/lib/inngest/client";
+import { sendSignUpEmail } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
 
 export const { GET, POST, PUT } = serve({ //background jobs
     client: inngest,
-    functions: [],
+    functions: [sendSignUpEmail],
 })
